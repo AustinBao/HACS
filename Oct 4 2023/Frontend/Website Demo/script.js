9aa4b1afc9464d68bf5cb78dcb7257f9f4914672
GithubConnoisseur <galileokim451@gmail.com>
2023-10-07 20:43:20 -0600

    organized the folders

--- Oct 4 2023/Frontend/Website Demo/script.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    // Add your login logic here
-    alert("Login button clicked");
-}
-
-function signup() {
-    // Add your signup logic here
-    alert("Sign Up button clicked");
-}
