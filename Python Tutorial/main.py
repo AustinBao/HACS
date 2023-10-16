192eef7f8db5fe50e4d6ae535a118bf186b45d88
AustinBao <austin_dun@outlook.com>
2023-10-16 17:09:18 -0600

    cleaned up

--- Python Tutorial/main.py
+++ Python Tutorial/main.py
@@ -1,29 +1,15 @@
-# Variables, loops, arrays, and if-statements 
-# schedule = []
-# block = 1
-
-# for i in range(4):
-#     classes = input(f"block {block}:")
-#     schedule.append(classes)
-#     print(schedule)
-#     block += 1
-
-# if "math" in schedule:
-#     print("you have math!")
-# elif "chemistry" in schedule:
-#     print("you have chemistry!")
-# else:
-#     print("you DONT have chemistry or math")
-
-schedule = []
+# Variables
+schedule = [] #---> an array (used to store information/variables)
 block = 1
 
+# FOR loop
 for i in range(4):
-    classes = input(f"block {block}:")
-    schedule.append(classes)
-    print(schedule)
-    block += 1
+    classes = input(f"block {block}:") # grabs user input (the classes they are taking in our context)
+    schedule.append(classes) # adds the "classes" user input into the array we made above
+    print(schedule) # printing out the result just for us to see
+    block += 1 # 
 
+# If, elif, and else statements (checks in subsequential order) 
 if "math" in schedule:
     print("you have math!")
 elif "chemistry" in schedule:
